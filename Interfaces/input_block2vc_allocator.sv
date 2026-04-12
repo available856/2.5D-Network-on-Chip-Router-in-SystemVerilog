f09a8e4e9d31ff0de4d3d922f08b8508c934e255
Nikolaos Charalampous <73706274+available856@users.noreply.github.com>
2026-04-12 22:52:44 +0300

    Add files via upload

--- Interfaces/input_block2vc_allocator.sv
+++ Interfaces/input_block2vc_allocator.sv
@@ -2,10 +2,10 @@ import noc_params::*;
 
 interface input_block2vc_allocator;
 
-    logic [VC_SIZE-1:0] vc_new [PORT_NUM-1:0] [VC_NUM-1:0];
-    logic [VC_NUM-1:0] vc_valid [PORT_NUM-1:0];
-    logic [VC_NUM-1:0] vc_request [PORT_NUM-1:0];
-    port_t [VC_NUM-1:0] out_port [PORT_NUM-1:0];
+    logic [VC_SIZE-1:0] vc_new [PORT_NUM-1:0] [VC_NUM-1:0];  // Allocated downstream VC index for each upstream agent (port, vc)
+    logic [VC_NUM-1:0] vc_valid [PORT_NUM-1:0];    //Upstream validation after allocation of downstream VC
+    logic [VC_NUM-1:0] vc_request [PORT_NUM-1:0];  //Upstream request for downstream VC allocation
+    port_t [VC_NUM-1:0] port_new [PORT_NUM-1:0];
     logic [PORT_NUM-1:0][VC_NUM-1:0][PORT_NUM-1:0] out_port_mask;
     logic [PORT_NUM-1:0][VC_NUM-1:0] credits_exist;
     vc_class_t [PORT_NUM-1:0][VC_NUM-1:0] vc_class;
@@ -13,18 +13,18 @@ interface input_block2vc_allocator;
     modport input_block (
         input vc_new,
         input vc_valid,
-        output vc_request,
-        output out_port
+        output vc_request
+  //      output out_port
     );
 
     modport vc_allocator (
         output vc_new,
         output vc_valid,
+        output port_new,
         input vc_request,
-        input out_port,
         input out_port_mask,
-        credits_exist,
-        vc_class
+        input credits_exist,
+        input vc_class
     );
 
 endinterface
